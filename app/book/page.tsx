716285dd5d569d27ef59ce41b0155bd88ec6b3de
NeilRow56 <neilrowland56@gmail.com>
2025-02-12 15:51:52 +0000

    Added skeleton pages for admin sections and full dashboard template

---
+++ app/book/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Book() {
+  return (
+    <section className='pb-24 pt-40'>
+      <div className='container max-w-5xl'>
+        <div>Booking Form</div>
+      </div>
+    </section>
+  )
+}
--- components/providers.tsx
+++ components/providers.tsx
@@ -12,7 +12,7 @@ export default function Providers({ children }: { children: React.ReactNode }) {
     <ThemeProvider
       enableSystem
       attribute='class'
-      defaultTheme='dark'
+      defaultTheme='sustem'
       disableTransitionOnChange
     >
       <Layout>{children}</Layout>
